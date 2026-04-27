12bcf439dda9b4e2f498db31d464c0d7c2415233
Fahrel arifqi <2511500089@mahasiswa.atmaluhur.ac.id>
2026-04-27 03:32:17 -0700

    data new

---
+++ index.php
@@ -0,0 +1,201 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "db_penjadwalan";
+$conn = mysqli_connect($host, $user, $pass, $db);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Starter</title>
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="index.php" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Contact</a>
+      </li>
+    </ul>
+
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+          <i class="fas fa-search"></i>
+        </a>
+        <div class="navbar-search-block">
+          <form class="form-inline">
+            <div class="input-group input-group-sm">
+              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+              <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit">
+                  <i class="fas fa-search"></i>
+                </button>
+                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                  <i class="fas fa-times"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="far fa-comments"></i>
+          <span class="badge badge-danger navbar-badge">3</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <a href="index3.html" class="brand-link">
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Jadwal Sekolah XYZ</span>
+    </a>
+
+    <div class="sidebar">
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Admin TU</a>
+        </div>
+      </div>
+
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          
+          <li class="nav-item">
+            <a href="index.php" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+
+          <li class="nav-item menu-open">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-database"></i>
+              <p>
+                DATA MASTER
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="guru.php" class="nav-link">
+                  <i class="far fa-circle nav-icon text-info"></i>
+                  <p>Data Guru</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="siswa.php" class="nav-link">
+                  <i class="far fa-circle nav-icon text-info"></i>
+                  <p>Data Siswa</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="mapel.php" class="nav-link">
+                  <i class="far fa-circle nav-icon text-info"></i>
+                  <p>Data Mapel</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="kelas.php" class="nav-link">
+                  <i class="far fa-circle nav-icon text-info"></i>
+                  <p>Data Kelas</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <li class="nav-item">
+            <a href="jadwal.php" class="nav-link">
+              <i class="nav-icon fas fa-calendar-alt"></i>
+              <p>Jadwal Pelajaran</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="user.php" class="nav-link">
+              <i class="nav-icon fas fa-user-cog"></i>
+              <p>Manajemen User</p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </aside>
+
+  <div class="content-wrapper">
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Halaman Utama</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+          </div>
+        </div>
+      </div> 
+    </div>
+
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">``
+          <div class="col-lg-3 col-6">
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>150</h3>
+                <p>Siswa</p>
+              </div>
+              <div class="icon"><i class="fas fa-users"></i></div>
+            </div>
+          </div>
+          <div class="col-lg-9">
+            <div class="card card-primary card-outline">
+              <div class="card-body">
+                <h5 class="card-title">Selamat Datang</h5>
+                <p class="card-text">
+                  Ini adalah Sistem Penjadwalan Sekolah XYZ. Silakan gunakan menu di samping untuk mengelola data.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-inline">
+      PWD - Terstruktur to OOP
+    </div>
+    <strong>Copyright &copy; 2026 <a href="#">Sekolah XYZ</a>.</strong> All rights reserved.
+  </footer>
+</div>
+
+<script src="plugins/jquery/jquery.min.js"></script>
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="dist/js/adminlte.min.js"></script>
+</body>
+</html>
